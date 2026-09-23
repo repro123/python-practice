7e42d021334f051dfeb96e528abc3f54335d7ee2
Ibrahim Bello <ibrahimopeyemibello@gmail.com>
2026-09-23 23:01:10 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,84 @@
+# import math
+
+# price = 10
+# rating = 4.9
+# name = "Bello"
+# is_published = True
+# print(price)
+
+# name = "John Smith"
+# age = 20
+# is_new = True
+# print()
+
+# name = input("What is your name? ")
+# print("Hi " + name)
+
+# birth_year = input("Birth year: ")
+# print(type(birth_year))
+# age = 2019 - int(birth_year)
+# print(type(age))
+# print(age)
+
+# string
+# course = 'Python for beginners'
+# another = course[:]
+# print(another)
+
+# first = "John"
+# last = "smith"
+# message = first + " [" + last + "] is a coder"
+# ms = f'{first} [{last}] is a coder'
+# print(ms)
+
+# course = "Python for beginners"
+# print(len(course))
+# print(course.lower())
+# print(course.upper())
+# print(course.find("P"))
+# print(course.replace("beginners", "Asolute beginners"))
+# print("Python" in course)
+
+# arithmetics
+# print(10 + 3)
+# print(10 / 3)
+# print(10 // 3)
+# print(10 % 3)
+# print(10**3)
+
+# math functions
+# print(round(2.9))
+# print(abs(2.9))
+# print(math.ceil(2.9))
+# print(math.floor(2.9))
+
+# if statements
+# is_hot = False
+# is_cold = True
+
+# if is_hot:
+#     print("It's a hot day")
+#     print("Drink plenty of water")
+# elif is_cold:
+#     print("It's a cold day")
+#     print("Wear warm")
+# else:
+#     print("Enjoy your day")
+# temp = 20
+# if temp > 30:
+#     print("It's a hot day")
+# else:
+#     print("It's not a hot day")
+
+# weight = float(input("Weight: "))
+# unit = input("(L)bs or (K)g: ")
+
+
+# if unit.upper() == "L":
+#     calc_weight = weight * 0.453592
+#     calc_unit = "kg"
+# else:
+#     calc_weight = weight * 2.20462
+#     calc_unit = "pounds"
+
+# print(f"You are {round(calc_weight)} {(calc_unit)}")
